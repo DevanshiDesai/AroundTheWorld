97edaa8d29c08ef0733a05d26f0fadf92c7d2e49
Uditi Mehta <57388785+uditijmehta@users.noreply.github.com>
2020-08-03 13:39:13 +0530

    Update login_Version3.py

--- streamlit_application/login_Version3.py
+++ streamlit_application/login_Version3.py
@@ -1,31 +1,40 @@
 import streamlit as st
 import pyodbc
 import datetime
-from PIL import Image
-cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=LAPTOP-FDBNNUQO;Database=AroundTheWorld;Trusted_Connection=yes;')
+from PIL import Image 
+
+cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=LAPTOP-4BH8IBHF\MSSQLSERVER01;Database=AroundTheWorld;Trusted_Connection=yes;')
 cursor = cnxn.cursor()
+
 def addCustomerData(CustFirstName,CustLastName,CustBirthDate,CustPhoneNo,CustStreetName,CustZipCode,CustEmail,CustPassword,CustAge,CustGender):
     cursor.execute('INSERT INTO Customer(CustFirstName,CustLastName,CustBirthDate,CustPhoneNo,CustStreetName,CustZipCode,CustEmail,CustPassword,CustAge,CustGender) VALUES (?,?,?,?,?,?,?,?,?,?)', (CustFirstName,CustLastName,CustBirthDate,CustPhoneNo,CustStreetName,CustZipCode,CustEmail,CustPassword,CustAge,CustGender))
+
 def addPreferenceData(CustBudget,PrefPackageType,CustID):
     cursor.execute('INSERT INTO CustomerPreference(CustBudget,PrefPackageType,CustID) VALUES (?,?,?)', (CustBudget,PrefPackageType,CustID))
+
 def addPreferenceCity(CityID,CustPrefID):
     cursor.execute('INSERT INTO CustomerPreferredCity(CityID,CustPrefID) VALUES (?,?)', (CityID,CustPrefID))   
+
 def loginCustomer(CustEmail,CustPassword):
     cursor.execute('SELECT * FROM Customer WHERE CustEmail=? AND CustPassword=?', (CustEmail,CustPassword))
     data = cursor.fetchall()
     return data
+
 def viewAllCustomers():
     cursor.execute('SELECT * FROM Customer')
     data = cursor.fetchall()
     return data
+
 def fetchCustIDbyEmail(CustEmail):
     cursor.execute('SELECT CustID FROM Customer WHERE CustEmail=?', (CustEmail))
     data = cursor.fetchall()
     return data
+
 def fetchCustPrefIDbyCustID(CustID):
     cursor.execute('SELECT CustPrefID FROM CustomerPreference WHERE CustID=?', (CustID))
     data = cursor.fetchall()
     return data
+
 def login():
     st.subheader("Login")
     username = st.text_input("Email")
@@ -39,6 +48,7 @@ def login():
             #st.info(CustID)
         else:
             st.warning("Invalid Username/Password")
+
 def signup():
     st.subheader("Create a New Account")
     CustFirstName = st.text_input("First Name")
@@ -55,6 +65,7 @@ def signup():
         addCustomerData(CustFirstName,CustLastName,CustBirthDate,CustPhoneNo,CustStreetName,CustZipCode,CustEmail,CustPassword,CustAge,CustGender)
         st.success("Congratulations!!! You have successfully created an account")
         login()
+
 def preference(CustID):
     st.subheader("Enter your preferences")
     CustBudget = st.text_input("Budget")
@@ -63,16 +74,17 @@ def preference(CustID):
         addPreferenceData(CustBudget,PrefPackageType,CustID)
         CustPrefID=fetchCustPrefIDbyCustID(CustID)
         preferenceCity(CustPrefID)
+
 def preferenceCity(CustPrefID):
     st.subheader("Enter your preferred cities")
     CustDestination = st.text_input("Preferred Destination")
     if st.button("Proceed"):
         st.info(CustPrefID)
         addPreferenceCity(CustDestination,CustPrefID) 
+
 def main():
-    
-  
-    menu = ["Home", "SignUp","Login","Preference","Package"]
+    """ Around The World - Travel Management System """
+    menu = ["Home", "SignUp","Login","Preference","City Preference","Package"]
     choice = st.sidebar.selectbox("Menu", menu)
     if choice == "Home":
         with open("style.css") as f:
@@ -80,17 +92,22 @@ def main():
             st.markdown("<h2 style='text-align: center; color: black;'>Around The World - A Travel Management System </h2>", unsafe_allow_html=True)
             st.markdown('<style>body{background-color: #CAFFCA;}</style>',unsafe_allow_html=True)
 
-        image = Image.open("C:\\Users\\lenovo\\Desktop\\Streamlit\\AroundTheWorld-master\\streamlit_application\\Logo.png")
+        image = Image.open("C:\\Users\\Logo.png")
         st.image(image, caption ='', width=None )
-    
     elif choice == "Login":
+        st.markdown('<style>body{background-color: #C9C9FF;}</style>',unsafe_allow_html=True)
         login()
     elif choice == "SignUp":
+        st.markdown('<style>body{background-color: #FFC9C9;}</style>',unsafe_allow_html=True)
         signup()
     elif choice == "Preference":
-        preference(9)
+        st.markdown('<style>body{background-color: #CAFFCA;}</style>',unsafe_allow_html=True)
+        preference(2)
     elif choice == "City Preference":
+        st.markdown('<style>body{background-color: #FFC9C9;}</style>',unsafe_allow_html=True)
         preferenceCity(5)
+    elif choice == "Package":
+        st.markdown('<style>body{background-color: #C9C9FF;}</style>',unsafe_allow_html=True)
     cnxn.commit()
     cnxn.close()
 main()
